4c8f98ed2f054f1ecf0c9b1130293a9ec9b62c6c
114qq <1149893855@qq.com>
2024-05-15 17:29:20 +0800

    添加router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,62 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "@/router";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+import {
+    Button,
+    ConfigProvider,
+    Layout,
+    Menu,
+    message,
+    Input,
+    Space,
+    Dropdown,
+    Divider,
+    Form,
+    AutoComplete,
+    Modal,
+    Tree,
+    Drawer,
+    Row,
+    Col,
+    Select,
+    DatePicker,
+    Tooltip,
+    Breadcrumb,
+    Popconfirm,
+    InputNumber,
+    Table,
+    Pagination,
+} from 'ant-design-vue'
+
+
+app.use(Button)
+    .use(Layout)
+    .use(ConfigProvider)
+    .use(Menu)
+    // .use(message)
+    .use(Input)
+    .use(Space)
+    .use(Dropdown)
+    .use(Divider)
+    .use(Form)
+    .use(AutoComplete)
+    .use(Modal)
+    .use(Tree)
+    .use(Drawer)
+    .use(Row)
+    .use(Col)
+    .use(Select)
+    .use(DatePicker)
+    .use(Tooltip)
+    .use(Breadcrumb)
+    .use(Popconfirm)
+    .use(InputNumber)
+    .use(Table)
+    .use(Pagination)
+    .use(router)
+    .mount('#app');
+
+app.config.globalProperties.$message = message;
+// createApp(App).mount('#app')
